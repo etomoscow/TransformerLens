b6ed3d2cdb2cc510181ce0919ed8258a9e70a5e5
Joel Burget <joelburget@gmail.com>
2024-12-14 06:37:59 -0800

    Implement TODO (norm_topk_prob)

--- transformer_lens/HookedTransformerConfig.py
+++ transformer_lens/HookedTransformerConfig.py
@@ -200,6 +200,7 @@ class HookedTransformerConfig:
             to None, which means the standard rotary_base is used for all layers.
 
 
+        norm_topk_prob (bool): Whether to normalize the top-k probabilities in the MoE layer.
     """
 
     n_layers: int
@@ -272,6 +273,7 @@ class HookedTransformerConfig:
     NTK_by_parts_low_freq_factor: float = 1.0
     NTK_by_parts_high_freq_factor: float = 4.0
     NTK_by_parts_factor: float = 8.0
+    norm_topk_prob: bool = False
     NTK_original_ctx_len: int = 8192
 
     def __post_init__(self):
