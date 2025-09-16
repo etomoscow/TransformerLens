97d39ad008c2dc64308f30a53e3a000b986c8c94
Bryce Meyer <bryce13950@gmail.com>
2025-09-16 23:19:51 +0200

    fixed doc string

--- transformer_lens/HookedTransformer.py
+++ transformer_lens/HookedTransformer.py
@@ -1412,6 +1412,7 @@ class HookedTransformer(HookedRootModule):
         if move_to_device:
             model.move_model_modules_to_device()
 
+        print(f"Loaded pretrained model {model_name} into HookedTransformer")
         return model
 
     @classmethod
